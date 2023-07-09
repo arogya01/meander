cbbb80885821acce0498c712e42fa233b4c18811
arogya bichpuria <arogya.bichpuria@bfhl.in>
2023-07-09 14:32:32 +0530

    scaffolded the react native app

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>yo this is going to be one hell of an ride!!</Text>
       <StatusBar style="auto" />
     </View>
   );
